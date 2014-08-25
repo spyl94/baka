673d2a14abaf32231666e6791d5f56b3c6fcbf69
spyl94 <spyl94@gmail.com>
2014-08-25 18:00:57 +0200

    use partials

--- web/assets/src/js/module.js
+++ web/assets/src/js/module.js
@@ -1,10 +1,25 @@
 'use strict';
 
 angular.module('baka', [
+  'ngRoute',
   'api',
   ])
-  .config(['$interpolateProvider', function ($interpolateProvider) {
+  .config(['$interpolateProvider', '$routeProvider', function ($interpolateProvider, $routeProvider) {
     $interpolateProvider.startSymbol('{[{');
     $interpolateProvider.endSymbol('}]}');
+
+    $routeProvider.
+      when('/', {
+        templateUrl: 'assets/src/partials/manga-list.html',
+        controller: 'MangasCtrl'
+      }).
+      when('/mangas/:id', {
+        templateUrl: 'assets/src/partials/manga-read.html',
+        controller: 'MangasCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+
 }]);
 angular.module('api', ['ngResource']);
